1a6f095383ce0d56639ab1df23d35c98f5cbc1ee
AjoloteCosmico <72818026+AjoloteCosmico@users.noreply.github.com>
2026-06-01 14:30:37 -0600

    xd

--- resources/views/admin/providers/create.blade.php
+++ resources/views/admin/providers/create.blade.php
@@ -44,13 +44,15 @@
                             </div>
 
                             <div class="form-group" id="fisica_name_group" style="display: none;">
+                                <x-jet-label value="* Apellido Paterno" />
+                                <x-jet-input type="text" name="last_name1" id="last_name1" class="w-full text-xs " value="{{old('last_name1')}}" onkeyup="javascript:this.value=this.value.toUpperCase();"/>
+                                <x-jet-input-error for='last_name1' />
+                                <x-jet-label value="* Apellido Materno" />
+                                <x-jet-input type="text" name="last_name2" id="last_name2" class="w-full text-xs " value="{{old('last_name2')}}" onkeyup="javascript:this.value=this.value.toUpperCase();"/>
+                                <x-jet-input-error for='last_name2' />
                                 <x-jet-label value="* Nombre" />
                                 <x-jet-input type="text" name="first_name" id="first_name" class="w-full text-xs " value="{{old('first_name')}}" onkeyup="javascript:this.value=this.value.toUpperCase();"/>
                                 <x-jet-input-error for='first_name' />
-                                <br>
-                                <x-jet-label value="* Apellidos" />
-                                <x-jet-input type="text" name="last_name" id="last_name" class="w-full text-xs " value="{{old('last_name')}}" onkeyup="javascript:this.value=this.value.toUpperCase();"/>
-                                <x-jet-input-error for='last_name' />
                             </div>
 
                             <div class="form-group" id="legal_name_group">
@@ -95,13 +97,34 @@
                             <div class="form-group" id="regimen_fiscal_group" style="display: none;">
                                 <x-jet-label value="* Regimen Fiscal" />
                                 <select class="form-capture  w-full text-xs uppercase" id="regimen_fiscal" name="regimen_fiscal">
-                               
-                                <option value="1" @if(old('regimen_fiscal')=='1') selected @endif> AC</option>
-                                <option value="2" @if(old('regimen_fiscal')=='2') selected @endif> SA de CV</option>
-                                <option value="3" @if(old('regimen_fiscal')=='3') selected @endif> S en N C</option>
-
 
                                     <option value="" > </option>
+                                    <option value="601" @if(old('regimen_fiscal')=='601') selected @endif>601 - REGIMEN GENERAL DE LEY PERSONAS MORALES</option>
+                                    <option value="602" @if(old('regimen_fiscal')=='602') selected @endif>602 - RÉGIMEN SIMPLIFICADO DE LEY PERSONAS MORALES</option>
+                                    <option value="603" @if(old('regimen_fiscal')=='603') selected @endif>603 - PERSONAS MORALES CON FINES NO LUCRATIVOS</option>
+                                    <option value="604" @if(old('regimen_fiscal')=='604') selected @endif>604 - RÉGIMEN DE PEQUEÑOS CONTRIBUYENTES</option>
+                                    <option value="605" @if(old('regimen_fiscal')=='605') selected @endif>605 - RÉGIMEN DE SUELDOS Y SALARIOS E INGRESOS ASIMILADOS A SALARIOS</option>
+                                    <option value="606" @if(old('regimen_fiscal')=='606') selected @endif>606 - RÉGIMEN DE ARRENDAMIENTO</option>
+                                    <option value="607" @if(old('regimen_fiscal')=='607') selected @endif>607 - RÉGIMEN DE ENAJENACIÓN O ADQUISICIÓN DE BIENES</option>
+                                    <option value="608" @if(old('regimen_fiscal')=='608') selected @endif>608 - RÉGIMEN DE LOS DEMÁS INGRESOS</option>
+                                    <option value="609" @if(old('regimen_fiscal')=='609') selected @endif>609 - RÉGIMEN DE CONSOLIDACIÓN</option>
+                                    <option value="610" @if(old('regimen_fiscal')=='610') selected @endif>610 - RÉGIMEN RESIDENTES EN EL EXTRANJERO SIN ESTABLECIMIENTO PERMANENTE EN MÉXICO</option>
+                                    <option value="611" @if(old('regimen_fiscal')=='611') selected @endif>611 - RÉGIMEN DE INGRESOS POR DIVIDENDOS (SOCIOS Y ACCIONISTAS)</option>
+                                    <option value="612" @if(old('regimen_fiscal')=='612') selected @endif>612 - RÉGIMEN DE LAS PERSONAS FÍSICAS CON ACTIVIDADES EMPRESARIALES Y PROFESIONALES</option>
+                                    <option value="613" @if(old('regimen_fiscal')=='613') selected @endif>613 - RÉGIMEN INTERMEDIO DE LAS PERSONAS FÍSICAS CON ACTIVIDADES EMPRESARIALES</option>
+                                    <option value="614" @if(old('regimen_fiscal')=='614') selected @endif>614 - RÉGIMEN DE LOS INGRESOS POR INTERESES</option>
+                                    <option value="615" @if(old('regimen_fiscal')=='615') selected @endif>615 - RÉGIMEN DE LOS INGRESOS POR OBTENCIÓN DE PREMIOS</option>
+                                    <option value="616" @if(old('regimen_fiscal')=='616') selected @endif>616 - SIN OBLIGACIONES FISCALES</option>
+                                    <option value="617" @if(old('regimen_fiscal')=='617') selected @endif>617 - PEMEX</option>
+                                    <option value="618" @if(old('regimen_fiscal')=='618') selected @endif>618 - RÉGIMEN SIMPLIFICADO DE LEY PERSONAS FÍSICAS</option>
+                                    <option value="619" @if(old('regimen_fiscal')=='619') selected @endif>619 - INGRESOS POR LA OBTENCIÓN DE PRÉSTAMOS</option>
+                                    <option value="620" @if(old('regimen_fiscal')=='620') selected @endif>620 - SOCIEDADES COOPERATIVAS DE PRODUCCIÓN QUE OPTAN POR DIFERIR SUS INGRESOS</option>
+                                    <option value="621" @if(old('regimen_fiscal')=='621') selected @endif>621 - RÉGIMEN DE INCORPORACIÓN FISCAL</option>
+                                    <option value="622" @if(old('regimen_fiscal')=='622') selected @endif>622 - RÉGIMEN DE ACTIVIDADES AGRÍCOLAS, GANADERAS, SILVÍCOLAS Y PESQUERAS PM</option>
+                                    <option value="623" @if(old('regimen_fiscal')=='623') selected @endif>623 - RÉGIMEN DE OPCIONAL PARA GRUPOS DE SOCIEDADES</option>
+                                    <option value="624" @if(old('regimen_fiscal')=='624') selected @endif>624 - RÉGIMEN DE LOS COORDINADOS</option>
+                                    <option value="625" @if(old('regimen_fiscal')=='625') selected @endif>625 - RÉGIMEN DE LAS ACTIVIDADES EMPRESARIALES CON INGRESOS A TRAVÉS DE PLATAFORMAS TECNOLÓGICAS</option>
+                                    <option value="626" @if(old('regimen_fiscal')=='626') selected @endif>626 - RÉGIMEN SIMPLIFICADO DE CONFIANZA</option>
                                     
                                     
                                 </select>
